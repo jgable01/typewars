2cd3fb1e1e9d19bb5b3d9b0136dd501f2f4a80fc
Josh <josh@gable.tech>
2023-03-23 17:26:05 -0500

    scorecard updates

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,12 +10,40 @@
 
 const startBtn = document.querySelector(".start");
 const info = document.querySelector(".info");
-const counter = document.querySelector('.counterp');
-const pointCount = document.querySelector('.points');
+const counter = document.querySelector(".counterp");
+const pointCount = document.querySelector(".points");
 const guessBox = document.querySelector(".guessBox");
 const wordBox = document.querySelector(".wordBox");
 const textField = document.getElementsByClassName("textField");
 const resetBtn = document.querySelector(".reset");
+const scoreSheet = document.querySelector(".scoreSheet");
+const date = document.querySelector("date");
+const hits = document.querySelector(".hits");
+const percentage = document.querySelector(".percentage");
+
+class Score {
+  #date;
+  #hits;
+  #percentage;
+
+  constructor(date, hits, percentage) {
+    this.#date = date;
+    this.#hits = hits;
+    this.#percentage = percentage;
+  }
+
+  get date() {
+    return this.#date;
+  }
+
+  get hits() {
+    return this.#hits;
+  }
+
+  get percentage() {
+    return this.#percentage;
+  }
+}
 
 const words = [
   "dinosaur",
@@ -113,10 +141,10 @@ const words = [
 let wordsTemp = [...words];
 let count = 0;
 let points = 0;
+let guesses = 0;
+let isActive = false;
 
 setInterval(update, 1000);
-
-
 
 startBtn.addEventListener("click", () => {
   startBtn.style.display = "none";
@@ -126,13 +154,18 @@ startBtn.addEventListener("click", () => {
   startGame();
 });
 
-guessBox.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
+resetBtn.addEventListener("click", () => {
+  resetGame();
+});
+
+guessBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    guesses += 1;
     if (guessBox.value.trim() === wordBox.innerHTML) {
       correctGuess();
     } else {
-      guessBox.style.border = '1px solid #ff3c57';
-      guessBox.value = '';
+      guessBox.style.border = "1px solid #ff3c57";
+      guessBox.value = "";
     }
   }
 });
@@ -142,10 +175,10 @@ function correctGuess() {
   console.log(`Index: ${wordIndex}`);
   wordsTemp.splice(wordIndex, 1);
   console.log(wordsTemp);
-  guessBox.style.border = '1px solid #4dcc3c';
+  guessBox.style.border = "1px solid #4dcc3c";
   wordBox.innerHTML = wordsTemp[randomNum()];
   points += 1;
-  guessBox.value = '';
+  guessBox.value = "";
 }
 
 function randomNum() {
@@ -153,19 +186,51 @@ function randomNum() {
 }
 
 function startGame() {
+  isActive = true;
   wordsTemp = [...words];
   console.log(wordsTemp.length);
   count = 99;
   points = 0;
+  guesses = 0;
   pointCount.innerHTML = `Points: ${points}`;
   counter.innerHTML = `Time Left: ${count}`;
   wordBox.innerHTML = wordsTemp[randomNum()];
 }
 
+function resetGame() {
+  isActive = false;
+  displayScore();
+  count = 0;
+  points = 0;
+  guesses = 0;
+  counter.innerHTML = `Time Left: ${count}`;
+  pointCount.innerHTML = `Points: ${points}`;
+  startBtn.style.display = "inline";
+  resetBtn.style.display = "none";
+  wordBox.style.display = "none";
+  guessBox.style.display = "none";
+}
+
+function displayScore() {
+  console.log(guesses);
+  const date = new Date();
+  const percent = (guesses / points);
+  date.toDateString();
+  const player = new Score(date, points, percent);
+  console.log(player);
+}
+
 function update() {
-  if (count != 0) {
+  if (count != 0 && isActive === true) {
     count -= 1;
     counter.innerHTML = `Time Left: ${count}`;
     pointCount.innerHTML = `Points: ${points}`;
+    guessBox.style.border = "";
+  } else {
+    if (isActive === true) {
+      displayScore();
+      resetGame();
+    }
+
   }
 }
